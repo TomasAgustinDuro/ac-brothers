e488677b58e7f6750bbd63252ad73c66138d92d8
Tomas <durotomaas@gmail.com>
2024-12-10 18:06:42 -0300

    Add responsive styles for desktop layout

--- src/sections/first/FirstSection.jsx
+++ src/sections/first/FirstSection.jsx
@@ -1,17 +1,18 @@
 import styles from "./FirstSection.module.css";
+import bro from '../../assets/brothers.jpeg'
 
 function FirstSection() {
   return (
     <div className={styles.firstSectionContainer}>
-      <h1>Perfect Climate Solutions for Your Home and Office</h1>
-      <p>
-      Repair, installation, and maintenance by two trusted experts
-      </p>
-      <button>
-        Contact us
-      </button>
+      <div className={styles.textContainer}>
+        <h1>Perfect Climate Solutions for Your Home and Office</h1>
+        <p>Repair, installation, and maintenance by two trusted experts</p>
+        <button>Contact us</button>
+      </div>
+
       <div>
-        Gallery
+        <img src={bro} alt="" />
+
       </div>
     </div>
   );
--- src/sections/third/ThirdSection.jsx
+++ src/sections/third/ThirdSection.jsx
@@ -1,17 +1,21 @@
 import styles from "./ThirdSection.module.css";
+import bro from "../../assets/brothers.jpeg";
 
 function ThirdSection() {
   return (
     <div className={styles.ThirdSectionContainer}>
-      <h2>About AC Brothers</h2>
-      <p>
-        With over 15 years of experience in the local market, AC Brothers is
-        your trusted partner for all air conditioning needs. Our commitment to
-        quality service and customer satisfaction sets us apart in the industry
-        As two brothers passionate about providing perfect climate solutions, we
-        bring expertise, reliability, and a personal touch to every job we
-        undertake.
-      </p>
+      <img src={bro} alt="" />
+      <div className={styles.TextContainer}>
+        <h2>About AC Brothers</h2>
+        <p>
+          With over 15 years of experience in the local market, AC Brothers is
+          your trusted partner for all air conditioning needs. Our commitment to
+          quality service and customer satisfaction sets us apart in the
+          industry As two brothers passionate about providing perfect climate
+          solutions, we bring expertise, reliability, and a personal touch to
+          every job we undertake.
+        </p>
+      </div>
     </div>
   );
 }
